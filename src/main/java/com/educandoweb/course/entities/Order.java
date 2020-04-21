272b71e027b9b1d72ef44df6f5690222a24491e3
Wleydson Lemos <wleydson.lemos@boavista.local>
2020-04-21 11:35:11 -0300

    BaseEntity

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,28 +1,21 @@
 package com.educandoweb.course.entities;
 
-import java.io.Serializable;
 import java.time.Instant;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.educandoweb.course.entities.base.BaseEntity;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name="tb_order")
-public class Order implements Serializable{
+public class Order extends BaseEntity{
 	
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
@@ -37,25 +30,13 @@ public class Order implements Serializable{
 		super();
 	}
 
-
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
-		this.id = id;
+		setId(id);
 		this.moment = moment;
 		setOrderStatus(orderStatus);
 		this.client = client;
 	}
-
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 
 	public Instant getMoment() {
 		return moment;
@@ -86,32 +67,5 @@ public class Order implements Serializable{
 		if(orderStatus != null)
 			this.orderStatus = orderStatus.getCode();
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Order other = (Order) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
 	
 }
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,27 +1,20 @@
 package com.educandoweb.course.entities;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.educandoweb.course.entities.base.BaseEntity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="tb_user")
-public class User implements Serializable{
+public class User extends BaseEntity{
 
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	
 	private String name;
 	
@@ -42,49 +35,12 @@ public class User implements Serializable{
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
-		this.id = id;
+		setId(id);
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
-	}	
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
 	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 
 	public String getName() {
 		return name;
